af195603457ba2792aa1f18d50cb4e87be21e3e2
NoumanEjaz-dev <noumanejaz488@gamil.com>
2026-03-21 02:05:09 +0500

    worked on reg api amd login api

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,13 @@
-const userModel = require('../models/user.models');
+const userModel = require('../models/user.models');  
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 /**
  * @name registerUserController
  * @description Register a new user. exppects username, email an password in the required 
  * @access Public 
  */
+
 
 async function registerUserController(req, res){
     const {username, email, password} = req.body;
@@ -25,10 +28,75 @@ async function registerUserController(req, res){
         })
     }
 
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign(
+        { id:user._id, username: user.name},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d"}
+        
+    )
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+        message:"User registered successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email:user.email
+        }
+
+    })
+ 
 }
 
 
+/**
+ * @name LoginUserController
+ * @description Login a new user. expects  email and password in the request body 
+ * @access Public 
+ */
+async function LoginUserController(req, res) {
+    const {email , password} =req.body
 
+    const user = await userModel.findOne({email})
+    
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid email or password",
+        })
+    }
+
+      const isPasswordValid = await bcrypt.compare(password , user.password)
+        
+       if(!isPasswordValid){
+            return res.status(400).json({
+                message: "Invalid email or password"
+            })
+        }
+
+    const token =jwt.sign(
+        {id:user._id, username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+    )
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message:"User loggedIn successfully",
+        user:user.email
+    })
+    
+} 
 module.exports ={
     registerUserController,
+    LoginUserController
 }
